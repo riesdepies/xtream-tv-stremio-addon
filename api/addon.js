9f9371a5acadda6fc0d6d53705a1342cf4b8bbd7
riesdepies <riesdepies@gmail.com>
2025-10-11 20:11:13 +0200

    Create addon.js

---
+++ api/addon.js
@@ -0,0 +1,76 @@
+const { addonBuilder, serveHTTP } = require('stremio-addon-sdk');
+
+// Data voor onze IPTV-zenders
+const iptvChannels = [
+    {
+        id: "iptv_1",
+        name: "Big Buck Bunny",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg",
+        streamUrl: "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4"
+    },
+    {
+        id: "iptv_2",
+        name: "Jellyfish 4K",
+        logo: "https://archive.org/download/jellies-4k-uhd-sample/jellies-4k-uhd-sample.jpg",
+        streamUrl: "https://archive.org/download/jellies-4k-uhd-sample/jellies-4k-uhd-sample.mp4"
+    },
+    {
+        id: "iptv_3",
+        name: "Sintel",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Sintel_poster_by_David_Revoy.jpg/220px-Sintel_poster_by_David_Revoy.jpg",
+        streamUrl: "https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f1/Sintel_movie_4K.webm/Sintel_movie_4K.webm.1080p.vp9.webm"
+    }
+];
+
+// Manifest
+const manifest = {
+    id: "org.iptvexample.vercel",
+    version: "1.0.0",
+    name: "IPTV Voorbeeld (Vercel)",
+    description: "Een simpele addon die IPTV zenders toont, gehost op Vercel.",
+    logo: "https://www.stremio.com/website/stremio-logo-small.png",
+    resources: ["catalog", "stream"],
+    types: ["tv"],
+    catalogs: [
+        {
+            type: "tv",
+            id: "iptv-zenders",
+            name: "Mijn TV Zenders"
+        }
+    ]
+};
+
+const builder = new addonBuilder(manifest);
+
+// Catalog Handler
+builder.defineCatalogHandler(args => {
+    if (args.type === 'tv' && args.id === 'iptv-zenders') {
+        const metas = iptvChannels.map(channel => ({
+            id: channel.id,
+            type: 'tv',
+            name: channel.name,
+            poster: channel.logo,
+            posterShape: 'landscape'
+        }));
+        return Promise.resolve({ metas: metas });
+    }
+    return Promise.resolve({ metas: [] });
+});
+
+// Stream Handler
+builder.defineStreamHandler(args => {
+    if (args.type === 'tv') {
+        const channel = iptvChannels.find(c => c.id === args.id);
+        if (channel) {
+            const stream = {
+                url: channel.streamUrl,
+                title: "Live"
+            };
+            return Promise.resolve({ streams: [stream] });
+        }
+    }
+    return Promise.resolve({ streams: [] });
+});
+
+// Exporteer de handler voor Vercel
+module.exports = serveHTTP(builder.getInterface());
